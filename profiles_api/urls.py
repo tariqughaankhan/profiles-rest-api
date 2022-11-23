d1fe34178f3320e11bf5d9c85c5f66ef7ba87273
tariq <cvai4272@gmail.com>
2022-11-23 10:52:58 +0000

    Helo apiView with post get and post

---
+++ profiles_api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from profiles_api import views
+
+
+urlpatterns=[
+
+
+     path('helo/',views.HelloApiView.as_view())
+ ]
---
+++ profiles_api/views.py
@@ -0,0 +1,40 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
+
+class HelloApiView (APIView):
+
+    """Test API View"""
+    serializer_class=serializers.HelloSerializer
+
+    def get(self, request, format=None):
+
+
+        an_apiview = [
+            'Uses HTTP methods as functions (get, post, patch, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over your logic',
+            'Is mapped manually to URLs',
+        ]
+
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+    def post(self, request ):
+
+         serializer=self.serializer_class(data=request.data)
+         if serializer.is_valid():
+             name=serializer.validated_data.get('name')
+             message=f'hello {name}'
+             return Response({'message':message})
+         else:
+             return Response(
+             serializer.errors,
+             status=status.HTTP_400_BAD_REQUEST
+             )
+
+    def put(request,pk=None):
+         return Response({'methods':'PUT'})
+    def patch(request,pk=None):
+         return Response({'methods':'PATCH'})
+    def delete(request,pk=None):
+         return Response({'methods':'Delete'}) 
