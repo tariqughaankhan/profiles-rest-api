d1b9b59d016392213ed8e7165d5ffef73f9e1953
Tariq <cvai4272@gmail.com>
2022-11-28 05:16:29 -0800

    API

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -5,11 +5,13 @@ from profiles_api import views
 router=DefaultRouter()
 router.register('hello-viewset',views.HelloViewSet, base_name='hello-viewset')
 router.register('Profile',views.UserProfileViewSet)
+router.register('feed',views.UserProfileFeedViewSet)
 
 urlpatterns=[
 
 
      path('helo/',views.HelloApiView.as_view()),
+      path('login/',views.UserLoginApiView.as_view()),
      path('',include(router.urls))
 
  ]
